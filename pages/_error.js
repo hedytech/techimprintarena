3d99903c7134643bd9d8e355dbcc738d8d6ced9c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-09-02 11:08:44 +0200

    Add error page

---
+++ pages/_error.js
@@ -0,0 +1,10 @@
+export default () => (
+  <div className='flex flex-wrap justify-center content-center h-screen bg-color-cover-full'>
+    <div className='w-full text-center'>
+      <h2 className='text-black text-xl font-bold'>404 - Page not found</h2>
+    </div>
+    <div className='w-full text-center pt-2'>
+      <p className=''><a href='/' title='Go to startpage' className='hover:underline'>Go to startpage</a></p>
+    </div>
+  </div>
+);
