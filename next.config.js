34f9bda866a36bd07be4fcbe98318d604b74ee99
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-09-02 13:53:46 +0200

    Delete dist

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
 const path = require('path');
 const withCSS = require('@zeit/next-css');
 const withOptimizedImages = require('next-optimized-images');
 module.exports = withCSS(withOptimizedImages({
+  exportTrailingSlash: isProd,
   webpack(config) {
     config.resolve.alias['static'] = path.join(__dirname, 'static');
     return config;
