b793d4f3aaf23926ab09c0721bb17c5720bdd5fd
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-09-02 14:00:14 +0200

    Redirect to slash

--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,25 @@
-export default () => (
-  <div className='flex flex-wrap justify-center content-center h-screen bg-color-cover-full'>
-    <div className='w-full text-center'>
-      <h2 className='text-black text-xl font-bold'>404 - Page not found</h2>
+import { useEffect } from 'react';
+
+export default () => {
+  useEffect(() => {
+    let pathname = window.location.pathname;
+    if (pathname[pathname.length - 1] === '/') {
+      window.location = pathname.slice(0, pathname.length - 1);
+    }
+  });
+
+  return (
+    <div className="flex flex-wrap justify-center content-center h-screen bg-color-cover-full">
+      <div className="w-full text-center">
+        <h2 className="text-black text-xl font-bold">404 - Page not found</h2>
+      </div>
+      <div className="w-full text-center pt-2">
+        <p className="">
+          <a href="/" title="Go to startpage" className="hover:underline">
+            Go to startpage
+          </a>
+        </p>
+      </div>
     </div>
-    <div className='w-full text-center pt-2'>
-      <p className=''><a href='/' title='Go to startpage' className='hover:underline'>Go to startpage</a></p>
-    </div>
-  </div>
-);
+  );
+};
