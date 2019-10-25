2aacefb86274d1f87318f72cd9ad0dcae8d782bd
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-10-25 11:30:19 +0200

    Add new logo and fix export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
+  exportTrailingSlash: true,
   webpack(config) {
     config.resolve.alias['public'] = path.join(__dirname, 'public');
     return config;
